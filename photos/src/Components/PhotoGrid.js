7f487f9b54cce3b4c845fe4667d2655745bfad53
Your Name <you@example.com>
2020-02-22 15:09:16 -0800

    got image displayed according to date change

--- photos/src/Components/PhotoGrid.js
+++ photos/src/Components/PhotoGrid.js
@@ -10,7 +10,7 @@ class PhotoGrid extends React.Component {
         }
     }
 
-    increaseDate(){
+    decreaseDate(){
         var currentDate = this.state.date
         for (let i = 0; i < currentDate.length; i++){
             var yyyy = currentDate.slice(0, 4)
@@ -37,8 +37,36 @@ class PhotoGrid extends React.Component {
 
     }
 
+    increaseDate(){
+        var currentDate = this.state.date
+        for (let i = 0; i < currentDate.length; i++){
+            var yyyy = currentDate.slice(0, 4)
+            var mm = currentDate.slice(5,7)
+            var dd = currentDate.slice(8,10)
+        }
+
+        var date = parseInt(dd)+1
+        var month = parseInt(mm)
+        var year = parseInt(yyyy)
+
+        if (date >= 30){
+            date = 0
+            month = month +1 
+        }
+        
+        if (month >= 12){
+            month = 1
+            year = year + 1
+        }
+
+        const tomorrow = String(year) + "-" + String(month).padStart(2, '0') + "-" + String(date).padStart(2, '0')
+        this.setState({date: tomorrow})
+
+    }
+
 
     componentDidMount(){
+        console.log('date in componentDidMount', this.state.date)
         axios 
         .get(`https://api.nasa.gov/planetary/apod?api_key=HpJEx1kZ3eD7MbBaHy0qDJKbMdjIEiAaprSj7Y8u&date=${this.state.date}`)
         .then(res => {
@@ -50,12 +78,37 @@ class PhotoGrid extends React.Component {
         
     }
 
+    fetchImages(){
+        console.log("hello")
+        axios 
+        .get(`https://api.nasa.gov/planetary/apod?api_key=HpJEx1kZ3eD7MbBaHy0qDJKbMdjIEiAaprSj7Y8u&date=${this.state.date}`)
+        .then(res => {
+            console.log(res)
+            this.setState({photo: res.data.url})
+            })
+        .catch(err=>{
+            console.log('err', err)})
+        
+        
+       }
+
 
     render(){
         console.log('the date', this.state.date)
         return (
             <div>
-                <button onClick={() => this.increaseDate()}>Next date</button>
+                <button onClick={() => 
+                {
+                 this.decreaseDate()
+                 this.fetchImages()
+                }
+                }>Time back</button>
+                <button onClick={() => 
+                {
+                 this.increaseDate()
+                 this.fetchImages()
+                }
+                }>Time forward</button>
                 <img src={this.state.photo}/>
             </div>
         )
