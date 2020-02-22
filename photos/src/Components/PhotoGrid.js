9cdeb93d131588e095c044bfbea5853e0351fa2e
Your Name <you@example.com>
2020-02-22 14:30:37 -0800

    got previous date

--- photos/src/Components/PhotoGrid.js
+++ photos/src/Components/PhotoGrid.js
@@ -10,6 +10,34 @@ class PhotoGrid extends React.Component {
         }
     }
 
+    increaseDate(){
+        var currentDate = this.state.date
+        for (let i = 0; i < currentDate.length; i++){
+            var yyyy = currentDate.slice(0, 4)
+            var mm = currentDate.slice(5,7)
+            var dd = currentDate.slice(8,10)
+        }
+
+        var date = parseInt(dd)-1
+        var month = parseInt(mm)
+        var year = parseInt(yyyy)
+
+        if (date <= 0){
+            date = 30
+            month = month -1 
+        }
+        
+        if (month <= 0){
+            month = 12
+            year = year - 1
+        }
+
+        const yesterday = String(year) + "-" + String(month).padStart(2, '0') + "-" + String(date).padStart(2, '0')
+        this.setState({date: yesterday})
+
+    }
+
+
     componentDidMount(){
         axios 
         .get(`https://api.nasa.gov/planetary/apod?api_key=HpJEx1kZ3eD7MbBaHy0qDJKbMdjIEiAaprSj7Y8u&date=${this.state.date}`)
@@ -24,8 +52,10 @@ class PhotoGrid extends React.Component {
 
 
     render(){
+        console.log('the date', this.state.date)
         return (
             <div>
+                <button onClick={() => this.increaseDate()}>Next date</button>
                 <img src={this.state.photo}/>
             </div>
         )
